8b3d6c2d184d622c89e22b7fd57be321b7925db4
TabMacedo1897 <106038097+TabMacedo1897@users.noreply.github.com>
2022-06-20 15:31:20 -0300

    Primeiro codigo do python
    pequenos comandos de python para teste

---
+++ codeflix/python.py
@@ -0,0 +1,9 @@
+print ('Digite o primeiro número:')
+x = input()
+
+print ('Digite o segundo numero')
+y = input ()
+
+resultado = x + y 
+
+print('O resultado da soma de ' + x + '+' + y + ' é igual a ' + resultado)
