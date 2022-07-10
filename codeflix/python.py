816f09b5fafb02f0ad1d235db6978ab1a275d766
TabMacedo1897 <106038097+TabMacedo1897@users.noreply.github.com>
2022-07-10 20:32:15 -0300

    Delete codeflix directory

--- codeflix/python.py
+++
@@ -1,9 +0,0 @@
-print ('Digite o primeiro número:')
-x = input()
-
-print ('Digite o segundo numero')
-y = input ()
-
-resultado = x + y 
-
-print('O resultado da soma de ' + x + '+' + y + ' é igual a ' + resultado)
